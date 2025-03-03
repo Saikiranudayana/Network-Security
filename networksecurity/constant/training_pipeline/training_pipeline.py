5a512de7ec63bc72873e1112e452753a2978b0c0
saikiranudayana <saikiranudayana@gmail.com>
2025-03-03 16:28:49 +0530

    yaml files

--- networksecurity/constant/training_pipeline/training_pipeline.py
+++ networksecurity/constant/training_pipeline/training_pipeline.py
@@ -16,6 +16,8 @@ FILE_NAME:str = "NetworkData.csv"
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME:str = "test.csv"
 
+SCHEMA_FILE_NAME:str = os.path.join("data_scheme","schema.yaml")
+
 """"
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """  
@@ -26,4 +28,15 @@ DATA_INGESTION_DATABASE_NAME:str = "SAI_KIRAN"
 DATA_INGESTION_DIR_NAME:str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
 DATA_INGESTION_INGESTED_DIR:str = "ingested"
-DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2  
+
+
+"""
+Data validation realated constant start with DATA_VALIDATION VAR NAME
+"""
+    
+DATA_VALIDATION_DIR_NAME:str = "data_validation"
+DATA_VALIDATION_VALID_DIR:str = "validation"
+DATA_VALIDATION_INVALID_DIR:str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = "report.yaml"
