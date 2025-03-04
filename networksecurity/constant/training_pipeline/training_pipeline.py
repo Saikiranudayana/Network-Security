8c58f0c7f0e466c6129fe8ac444477e9aaeb0f69
saikiranudayana <saikiranudayana@gmail.com>
2025-03-04 11:05:42 +0530

    Data Transformation completed

--- networksecurity/constant/training_pipeline/training_pipeline.py
+++ networksecurity/constant/training_pipeline/training_pipeline.py
@@ -40,6 +40,8 @@ DATA_VALIDATION_DIR_NAME:str = "data_validation"
 DATA_VALIDATION_VALID_DIR:str = "validation"
 DATA_VALIDATION_INVALID_DIR:str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME:str="preprocessing.pkl"
  
  
 
