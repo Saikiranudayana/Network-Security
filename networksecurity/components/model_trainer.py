e01c4c2cd27746a7d69ec0c57a35a355905bf743
saikiranudayana <saikiranudayana@gmail.com>
2025-03-04 14:08:49 +0530

    mlflow

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -24,7 +24,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier
 )
-
+import mlflow
 
 
 class ModedlTrainer:
@@ -34,6 +34,23 @@ class ModedlTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise NetworkSecuirtyException(e,sys)
+        
+        
+        
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+            
+            mlflow.log_metric("f1 score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+            
+            
+            
+            
     def train_model(self, X_train, y_train, x_test, y_test):
         models = {
             "Random Forest": RandomForestClassifier(verbose=1),
@@ -94,10 +111,14 @@ class ModedlTrainer:
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
         
         ##Track the mlfow
+        self.track_mlflow(best_model,classification_train_metric)
+        
+        
         
         y_test_pred = best_model.predict(x_test)
         classification_test_metrics = get_classification_score(y_true=y_test,y_pred=y_test_pred)
         
+        self.track_mlflow(best_model,classification_test_metrics)
         preprocessor = load_object(file_path= self.data_transformation_artifact.transformed_object_file_path)
         
         model_dir_path = os.path.dirname(self.model_trainer_config.trained_model_file_path)
